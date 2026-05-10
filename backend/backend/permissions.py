2a1d3e7cce59d43cf49d807850c14d692c3142ef
EgroKuz <149324333+EgroKuz@users.noreply.github.com>
2026-05-10 16:03:47 +0300

    Remove README.md per request

---
+++ backend/backend/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsModeratorOrReadOnly(BasePermission):
+    """Allow read for everyone, write only for moderators (is_staff)."""
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_authenticated and request.user.is_staff)
--- backend/videos/views.py
+++ backend/videos/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, filters, permissions
 from django_filters.rest_framework import DjangoFilterBackend
+from django.views.generic import TemplateView
 from .models import Video, Author, UserVideoInteraction
+from backend.permissions import IsModeratorOrReadOnly
 from .serializers import VideoSerializer, AuthorSerializer, UserVideoInteractionSerializer
 
 class VideoViewSet(viewsets.ModelViewSet):
@@ -9,6 +11,7 @@ class VideoViewSet(viewsets.ModelViewSet):
     """
     queryset = Video.objects.filter(moderated=True).order_by('-uploaded_at')
     serializer_class = VideoSerializer
+    permission_classes = [IsModeratorOrReadOnly]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['video_type', 'game']
     search_fields = ['title', 'tags', 'author__name']
@@ -20,6 +23,7 @@ class AuthorViewSet(viewsets.ModelViewSet):
     """
     queryset = Author.objects.all().order_by('-subscribers_count')
     serializer_class = AuthorSerializer
+    permission_classes = [IsModeratorOrReadOnly]
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
 
@@ -35,3 +39,7 @@ class UserVideoInteractionViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class VideoListPageView(TemplateView):
+    """Страница просмотра видеоконтента."""
+    template_name = "videos/list.html"
